0ce0dd168d9c7219f7c304a349055282201e4a52
Hai <hainguyen10062000@gmail.com>
2021-08-30 17:59:23 +0700

    update

---
+++ ReactJS/src/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Nav = () => (
+    <nav>
+        <h1>Logo</h1>
+        <ul>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+        </ul>
+    </nav>
+)
+
+export default Nav;
