921964583378c0c4c30dd1d32e37ae0c14b94ab2
Hai <hainguyen10062000@gmail.com>
2021-09-01 10:21:18 +0700

    React-exercise1

---
+++ React-exercise1/exercise1/src/components/widgetSm/WidgetSm.jsx
@@ -0,0 +1,67 @@
+import "./widgetSm.css"
+import {Visibility} from "@material-ui/icons"
+
+export default function WidgetSm() {
+    return (
+        <div className="widgetSm">
+            <span className="widgetSmTitle">New Join Members</span>
+            <ul className="widgetSmList">
+                <li className="widgetSmListItem">
+                    <img src="https://images.unsplash.com/photo-1630292041337-d9d5886a7b0a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80" alt="" className="widgetSmImg" />
+                    <div className="widgetSmUser">
+                        <span className="widgetSmUserName">Anna Keller</span>
+                        <span className="widgetSmUserTitle">Software Engineer</span>
+                    </div>
+                    <button className="widgetSmButton">
+                        <Visibility className="widgetSmIcon"/>
+                        Display
+                    </button>
+                </li>
+                <li className="widgetSmListItem">
+                    <img src="https://images.unsplash.com/photo-1630292041337-d9d5886a7b0a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80" alt="" className="widgetSmImg" />
+                    <div className="widgetSmUser">
+                        <span className="widgetSmUserName">Anna Keller</span>
+                        <span className="widgetSmUserTitle">Software Engineer</span>
+                    </div>
+                    <button className="widgetSmButton">
+                        <Visibility className="widgetSmIcon"/>
+                        Display
+                    </button>
+                </li>
+                <li className="widgetSmListItem">
+                    <img src="https://images.unsplash.com/photo-1630292041337-d9d5886a7b0a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80" alt="" className="widgetSmImg" />
+                    <div className="widgetSmUser">
+                        <span className="widgetSmUserName">Anna Keller</span>
+                        <span className="widgetSmUserTitle">Software Engineer</span>
+                    </div>
+                    <button className="widgetSmButton">
+                        <Visibility className="widgetSmIcon"/>
+                        Display
+                    </button>
+                </li>
+                <li className="widgetSmListItem">
+                    <img src="https://images.unsplash.com/photo-1630292041337-d9d5886a7b0a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80" alt="" className="widgetSmImg" />
+                    <div className="widgetSmUser">
+                        <span className="widgetSmUserName">Anna Keller</span>
+                        <span className="widgetSmUserTitle">Software Engineer</span>
+                    </div>
+                    <button className="widgetSmButton">
+                        <Visibility className="widgetSmIcon"/>
+                        Display
+                    </button>
+                </li>
+                <li className="widgetSmListItem">
+                    <img src="https://images.unsplash.com/photo-1630292041337-d9d5886a7b0a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80" alt="" className="widgetSmImg" />
+                    <div className="widgetSmUser">
+                        <span className="widgetSmUserName">Anna Keller</span>
+                        <span className="widgetSmUserTitle">Software Engineer</span>
+                    </div>
+                    <button className="widgetSmButton">
+                        <Visibility className="widgetSmIcon"/>
+                        Display
+                    </button>
+                </li>
+            </ul>
+        </div>
+    )
+}
